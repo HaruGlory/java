48ffa5cd556dfb4c2aaf9d4daf54b2c197056e40
HaruGlory <71491458+HaruGlory@users.noreply.github.com>
2020-11-24 16:52:01 +0200

    Update DataTest.java

--- src/test/lr6/DataTest.java
+++ src/test/lr6/DataTest.java
@@ -8,11 +8,11 @@ public class DataTest {
     public void insertCheck() {
         DataBaseManager manager = new DataBaseManager();
         try {
-            manager.create("Daniil", "Mokhov", "Dmytrovych", "1999-11-01");
-            manager.create("Alexander", "Prokopenko", "Alexandrovych", "1998-01-01");
-            manager.create("Alex", "Nikitenko", "Olegovych", "1998-11-03");
-            manager.create("Marina", "Prokopenko", "Alexandrovna", "1998-01-01");
-            manager.searchByBirthday("1998-01-01");
+            manager.create("Андрей", "Андреев", "Олегович", "1999-12-01");
+            manager.create("Саша", "Васильев", "Антонович", "2000-05-28");
+            manager.create("Сергей", "Матвиенко", "Фёдорович", "1995-09-03");
+            manager.create("Алина", "Пупкина", "Владимировна", "1998-11-09");
+            manager.searchByBirthday("1998-11-09");
             manager.clearTable();
         } finally {
             manager.exit();
